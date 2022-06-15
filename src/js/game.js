c840e7de48325aa8165611bc8e4f406f29f601c2
Alexis1476 <halexisr20@hotmail.com>
2022-06-15 09:02:16 +0200

    Optimisation du code + commentaires

--- src/js/game.js
+++ src/js/game.js
@@ -1,143 +1,103 @@
-let timerSpan = document.getElementById('timer');
-let correctAnswersSpan = document.getElementById('correct-answers');
-let currentAnswerSpan = document.getElementById('current-answer');
-let nameCountry = document.getElementById('game-country-name');
-let flagCountry = document.getElementById('country-flag');
-let inputCapital = document.getElementById('country-capital');
-const TIMETOANSWER = 15; // Temps de réponse par question
-const NBANSWERS = 15;
-let time = TIMETOANSWER;
-let countriesToFind;
-let currentCountry;
-let nbCorrectAnswers = 0;
-let nbCurrentAnswer = 1;
-let interval = setInterval(decreaseTime, 1000);
+const TIMER_SPAN = document.getElementById('timer'); // Span qui contient le minuteur
+const OK_ANSWERS_SPAN = document.getElementById('correct-answers'); // Span qui affiche les réponses correctes
+const CURRENT_QUESTION_SPAN = document.getElementById('current-answer'); // Span qui affiche la question COURANTE
+const COUNTRY_NAME = document.getElementById('game-country-name'); // Balise contentant le nom du pays
+const COUNTRY_FLAG = document.getElementById('country-flag'); // Img contenant le drapeau du pays
+const CAPITAL_INPUT = document.getElementById('country-capital'); // Champ pour écrire la réponse
+const MODAL = document.getElementById('container-modal'); // Modal qui affiche la réponse correcte
+const MODAL_P = MODAL.getElementsByTagName('p'); // Paragraphe du modal contenant le message à afficher
+const QUESTION_TIME = 15; // Temps de réponse
+const NB_QUESTIONS = 15; // Nombre de questions
 
-inputCapital.addEventListener('keydown', submitAnswer);
+let countries; // Liste des pays
+let timer = QUESTION_TIME; // Minuteur
+let currentCountry; // Pays courant
+let nbOkAnswers = 0; // Nombre de réponses correctes
+let nbCurrentQuestion = 1; // Nombbre de la réponse courante
+let interval = setInterval(decreaseTime, 1000); // Interval qui gère le minuteur
 
+// Décrémente le temps de chaque réponse
 function decreaseTime() {
-    // Changement de couleur et de secondes
-    timerSpan.innerText = `${time} S`;
-    timerSpan.style.color = changeColor(time);
+    // Changer la couleur du minuteur
+    TIMER_SPAN.innerText = `${timer} S`;
+    TIMER_SPAN.style.color = timer <= 5 ? 'red' : 'white';
 
-    // Vérification du compteur
-    if (time <= 0) {
-        time = TIMETOANSWER;
-        // TODO: Afficher réponse correcte si incorrect
+    // Si le timer arrive au bout
+    if (timer <= 0) {
+        timer = QUESTION_TIME; // Reinitialise le timer
         checkAnswer();
-    } else {
-        time--;
-    }
+    } else timer--;
 }
 
-function submitAnswer() {
-    if (event.key === 'Enter' && isAWord(inputCapital.value)) {
-        checkAnswer();
-    }
-}
-
+// Vérifie la réponse de l'utilisateur
 function checkAnswer() {
-    // Si réponse correcte
-    if (inputCapital.value.localeCompare(currentCountry.capital[0], undefined, {sensitivity: 'base'}) === 0) {
-        nbCurrentAnswer++;
-        nbCorrectAnswers++;
+    nbCurrentQuestion++;
+
+    // Si la réponse est correcte sans avoir en compte les caractères accentués
+    if (CAPITAL_INPUT.value.localeCompare(currentCountry.capital[0], undefined, {sensitivity: 'base'}) === 0) {
+        nbOkAnswers++;
         updateUI();
     } else {
-        nbCurrentAnswer++;
-        // TODO: Optimiser
-        showAnswer(currentCountry.capital);
+        displayModal(`Oups! La réponse c'était : ${currentCountry.capital}`);
     }
+
+    // Si c'était la dernière question
+    if (nbCurrentQuestion === NB_QUESTIONS) gameIsOver();
 }
 
-function check() {
-    // Vérifier si c'est un mot
-    // Si c'est correct
-    // Popup réponse correcte + btn continuer
-    // Sinon
-    // Popup affichant la réponse correcte + btn continuer
-    // Arreter minuteur
-    // Si utilisateur appui continuer
-    // Reinitialiser minuteur
-    // Mettre à jour l'interface
+// Vérifie la réponse de l'utilisateur quand la touche Enter est tapée
+function submitAnswer() {
+    if (event.key === 'Enter' && isAWord(CAPITAL_INPUT.value)) checkAnswer();
 }
 
-function showAnswer(answer) {
-    let modal = document.getElementById('container-modal');
-    let children = modal.getElementsByTagName('p');
-    children[0].innerText = `Oups! La réponse c'était : ${answer}`;
-    modal.style.display = 'block';
+// Affiche le modal avec un mesage donné
+function displayModal(message) {
+    MODAL_P[0].innerText = message;
+    MODAL.style.display = 'block';
     clearInterval(interval);
 }
 
+// Ferme le modal
 function closeModal() {
-    const modal = document.getElementById('container-modal');
-    modal.style.display = 'none';
+    MODAL.style.display = 'none';
     interval = setInterval(decreaseTime, 1000);
     updateUI();
 }
 
+// Met à jour les éléments de l'interface
 function updateUI() {
-    inputCapital.value = '';
-    time = TIMETOANSWER;
+    CAPITAL_INPUT.value = '';
+    timer = QUESTION_TIME;
     updateCountry();
-    gameIsOver();
-    updateCounters();
+    updateCountersUI();
 }
 
+// Affiche un modal à la fin du jeu et reinitialise les compteurs
 function gameIsOver() {
-    if (nbCurrentAnswer === NBANSWERS) {
-        let modal = document.getElementById('container-modal');
-        let children = modal.getElementsByTagName('p');
-        children[0].innerText = `Vous avez répondu ${nbCorrectAnswers} réponses correctes sur ${NBANSWERS}`;
-        modal.style.display = 'block';
-        clearInterval(interval);
-        nbCorrectAnswers = 0;
-        nbCurrentAnswer = 1;
-        time = TIMETOANSWER;
-    }
+    displayModal(`Vous avez répondu ${nbOkAnswers} réponses correctes sur ${NB_QUESTIONS}`);
+    clearInterval(interval);
+    nbCurrentQuestion = 1;
+    nbOkAnswers = 0;
+    timer = QUESTION_TIME;
 }
 
-// TODO : Function repetée
-function isAWord(string) {
-    return string.match(/^\D+$/);
+// Met à jour les compteurs de réponses de l'interface
+function updateCountersUI() {
+    CURRENT_QUESTION_SPAN.innerText = `${nbCurrentQuestion} / ${NB_QUESTIONS}`;
+    OK_ANSWERS_SPAN.innerText = `${nbOkAnswers} / ${NB_QUESTIONS}`;
 }
 
-function updateCounters() {
-    currentAnswerSpan.innerText = `${nbCurrentAnswer} / ${NBANSWERS}`;
-    correctAnswersSpan.innerText = `${nbCorrectAnswers} / ${NBANSWERS}`;
-}
-
-function changeColor(timer) {
-    if (timer <= 5) {
-        return 'red';
-    } else {
-        return 'white';
-    }
-}
-
-
-async function getCountries(url) {
-    try {
-        let res = await fetch(url);
-        return await res.json();
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-async function initGame() {
-    let url = 'https://restcountries.com/v3.1/all?fields=name,capital,flags';
-    countriesToFind = await getCountries(url);
-    updateCountry();
-    updateCounters();
-}
-
+// Met à jour le pays courant
 function updateCountry() {
-    // Changer le pays
-    currentCountry = countriesToFind[Math.floor(Math.random() * countriesToFind.length)];
-    flagCountry.setAttribute('src', currentCountry.flags.png)
-    nameCountry.innerText = currentCountry.name.common;
-    console.log(currentCountry.capital);
+    currentCountry = countries[Math.floor(Math.random() * countries.length)];
+    COUNTRY_FLAG.setAttribute('src', currentCountry.flags.png)
+    COUNTRY_NAME.innerText = currentCountry.name.common;
 }
 
-initGame();
+// Initialise le jeu
+async function initGame() {
+    countries = await requestAPI(URL_ALL);
+    updateUI();
+}
+
+initGame().then();
